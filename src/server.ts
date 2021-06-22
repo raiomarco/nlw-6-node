a789909fcaac99fb387f615a45001855f298ffe9
raiomarco <raiomarco@piratasdoraio.com>
2021-06-22 00:03:20 -0300

    Atualizado servidor base para deixar limpo

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
+import "reflect-metadata"
 import express from 'express';
+import { router } from "./routes"
 
-// @types/express
+import "./database"
+
 const app = express();
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação especifica
- */
+app.use(express.json());
 
-app.get('/test', (req, res) => {
-    // req => Entrando
-    // res => Saindo
-    return res.send("Olá NLW");
-});
+app.use(router);
 
-app.post('/test-post', (req, res) => {
-    return res.send("Olá NLW método POST");
-})
-
-// http://localhost:3000/
 app.listen(3000, () => console.log("Server is running"));
