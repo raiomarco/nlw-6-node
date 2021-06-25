0948bb451d09273c1b4c9b6997b397353231b449
raiomarco <raiomarco@piratasdoraio.com>
2021-06-25 14:40:08 -0300

    Criado endpoint para login e auth de User

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController"
 import { CreateTagController } from "./controllers/CreateTagController"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
 
 import { ensureAdmin } from "./middlewares/ensureAdmin"
 
@@ -8,8 +9,10 @@ const router = Router();
 
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
+const authenticateUserController = new AuthenticateUserController()
 
 router.post("/users", createUserController.handle)
 router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/login", authenticateUserController.handle)
 
 export { router }
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories'
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+interface IAuthenticateRequest {
+    email: string;
+    password: string;
+}
+
+class AuthenticateUserService {
+    async execute({ email, password }: IAuthenticateRequest) {
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        const user = await usersRepository.findOne({ email });
+
+        if (!user) {
+            throw new Error("Email/Password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password)
+
+        if (!passwordMatch) {
+            throw new Error("Email/Password incorrect!");
+        }
+
+        const token = sign(
+            {
+                email: user.email
+            }, "e0d9d3862dfb270de65719d43749df5e",
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        )
+
+        return token
+    }
+}
+
+export { AuthenticateUserService }
