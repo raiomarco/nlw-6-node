6723da2e0d7204cd59119a226fb497cdb2666152
raiomarco <raiomarco@piratasdoraio.com>
2021-06-25 19:07:17 -0300

    novas rotas adicionadas e middleware de auth

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,11 @@ import { AuthenticateUserController } from "./controllers/AuthenticateUserContro
 import { CreateComplimentController } from "./controllers/CreateComplimentController";
 
 import { ensureAdmin } from "./middlewares/ensureAdmin"
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
+import { ListUserSentComplimentsController } from "./controllers/ListUserSentComplimentsController";
+import { ListUserReceivedComplimentsController } from "./controllers/ListUserReceivedComplimentsController";
+import { ListTagsController } from "./controllers/ListTagsController";
+import { ListUsersController } from "./controllers/ListUsersController";
 
 const router = Router();
 
@@ -12,10 +17,18 @@ const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 const authenticateUserController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const listUserSentComplimentsController = new ListUserSentComplimentsController()
+const listUserReceivedComplimentsController = new ListUserReceivedComplimentsController()
+const listTagsController = new ListTagsController()
+const listUsersController = new ListUsersController()
 
 router.post("/users", createUserController.handle)
-router.post("/tags", ensureAdmin, createTagController.handle)
+router.post("/tags", ensureAuthenticated, ensureAdmin, createTagController.handle)
 router.post("/login", authenticateUserController.handle)
-router.post("/compliments", createComplimentController.handle)
+router.post("/compliments", ensureAuthenticated, createComplimentController.handle)
+router.get("/users/compliments/sent", ensureAuthenticated, listUserSentComplimentsController.handle)
+router.get("/users/compliments/received", ensureAuthenticated, listUserReceivedComplimentsController.handle)
+router.get("/tags", ensureAuthenticated, listTagsController.handle)
+router.get("/users", ensureAuthenticated, listUsersController.handle)
 
 export { router }
