ad0ca5b25815f97c74ca24619a654f54fd7d55f2
raiomarco <raiomarco@piratasdoraio.com>
2021-06-25 19:04:07 -0300

    Corrigido validação de admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,7 +1,14 @@
 import { Request, Response, NextFunction } from "express"
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
-export function ensureAdmin(req: Request, res: Response, next: NextFunction) {
-    const admin = true
+export async function ensureAdmin(req: Request, res: Response, next: NextFunction) {
+    const { userId } = req;
+
+    // TODO: Criar service para acesso do repository
+    const usersRepository = getCustomRepository(UsersRepositories)
+
+    const { admin } = await usersRepository.findOne(userId);
 
     if (admin) {
         return next()
